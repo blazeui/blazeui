44f869e784f1d4db610f963ec7fcadd4cfe76be2
jankapunkt <jkuester@uni-bremen.de>
2024-10-22 08:40:32 +0200

    feature(components): Anchor classes are optional if href starts with hash

--- components/anchor/Anchor.js
+++ components/anchor/Anchor.js
@@ -1,21 +1,27 @@
 import './Anchor.html'
 
 /**
- * A lightweight link anchor that renders a hash (#) character on hover.
- * Use this for document-internal links.
+ * A lightweight link anchor that renders a hash (#) character on hover, if the href starts with a #.
  * @module
  * @see http://blazeui.meteorapp.com/components?c=Anchor
  */
 
 /**
-
  * @property href {string} anchor value, such as `#foo`
  */
 export const Anchor = {
   name: 'Anchor',
   main: true,
-  class: 'no-underline hover:underline hover:decoration-2 hover:after:ml-1 after:!no-underline hover:after:content-[\'#\'] hover:after:text-muted-foreground',
-  attributes: {
-    href: ''
+  class: 'no-underline hover:underline hover:decoration-2 hover:after:ml-1 after:!no-underline',
+  attributes ({ props, api }) {
+    const { merge } = api.styles()
+    const local = props.href.startsWith('#')
+    return {
+      href: props.href,
+      class: merge(
+        Anchor.class,
+        local && 'hover:after:content-[\'#\'] hover:after:text-muted'
+      )
+    }
   }
 }
